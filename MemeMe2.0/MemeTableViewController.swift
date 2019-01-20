2cc01164b1ad34e20e029b06be018d41e20bd298
Sabrina Svedin <sabrina.svedin@gmail.com>
2019-01-20 09:40:14 -0800

    Reload tableview in order to populate tableVC

--- MemeMe2.0/MemeTableViewController.swift
+++ MemeMe2.0/MemeTableViewController.swift
@@ -15,6 +15,11 @@ class MemeTableViewController: UITableViewController {
         let appDelegate = object as! AppDelegate
         return appDelegate.memes
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        tableView.reloadData()
+    }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return self.memes.count
