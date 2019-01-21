7ce347e2ceb90ac79f05c21e14bf1424bafc18ab
Sabrina Svedin <sabrina.svedin@gmail.com>
2019-01-21 10:13:30 -0800

    Collection View Flow Layout

--- MemeMe2.0/MemeCollectionViewController.swift
+++ MemeMe2.0/MemeCollectionViewController.swift
@@ -10,10 +10,23 @@ import UIKit
 
 class MemeCollectionViewController: UICollectionViewController {
 
+    @IBOutlet weak var flowLayout: UICollectionViewFlowLayout!
+    
     var memes: [Meme]! {
         let object = UIApplication.shared.delegate
         let appDelegate = object as! AppDelegate
         return appDelegate.memes
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let space:CGFloat = 3.0
+        let dimension = (view.frame.size.width - (2 * space)) / 3.0
+        
+        flowLayout.minimumInteritemSpacing = space
+        flowLayout.minimumLineSpacing = space
+        flowLayout.itemSize = CGSize(width: dimension, height: dimension)
     }
     
     override func viewWillAppear(_ animated: Bool) {
