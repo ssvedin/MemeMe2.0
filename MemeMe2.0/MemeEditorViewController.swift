cbb3dff08fc9394f9bdd2f13f10491cb3f3bacb1
Sabrina Svedin <sabrina.svedin@gmail.com>
2019-01-22 20:58:23 -0800

    Use Impact font

--- MemeMe2.0/MemeEditorViewController.swift
+++ MemeMe2.0/MemeEditorViewController.swift
@@ -78,7 +78,7 @@ class MemeEditorViewController: UIViewController, UIImagePickerControllerDelegat
         let memeTextAttributes: [NSAttributedString.Key: Any] = [
             NSAttributedString.Key.strokeColor: UIColor.black,
             NSAttributedString.Key.foregroundColor: UIColor.white,
-            NSAttributedString.Key.font: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
+            NSAttributedString.Key.font: UIFont(name: "Impact", size: 40)!,
             NSAttributedString.Key.strokeWidth: -4.0
         ]
         
